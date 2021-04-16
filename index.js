a8039f47e05cf289949acbda64ac008929825054
developersamuel <samuel@mecode.asia>
2021-04-16 14:31:59 +0700

    fix: changed name of menu button

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const getShit = document.getElementById("menuElement");
+const menuButton = document.getElementById("menuElement");
 const mainMenu = document.querySelector(".mainMenu");
 const openMenu = document.querySelector(".openMenu");
 
-getShit.addEventListener("click", show);
+menuButton.addEventListener("click", show);
 
 function show() {
-  if (!getShit.classList.contains("is-active")) {
-    getShit.classList.add("is-active");
+  if (!menuButton.classList.contains("is-active")) {
+    menuButton.classList.add("is-active");
     mainMenu.style.display = "flex";
     mainMenu.style.top = "0";
   } else {
-    getShit.classList.remove("is-active");
+    menuButton.classList.remove("is-active");
     mainMenu.style.top = "-100%";
   }
 }
